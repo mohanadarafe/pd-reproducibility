2fc193c75add5b7888e2e84b6fd9549c8b3562d8
mohanadarafe <marafe@live.com>
2021-11-08 23:05:30 -0500

    Extract volumes from recon-all aseg_stats to CSV format
    Document functions

--- preprocess.py
+++ preprocess.py
@@ -1,24 +1,25 @@
 import utils, os
 
 def prep_data(raw_dir, out_dir):
+    
     # Convert archive DICOM to BIDS
-    # utils.convert_data_to_bids(raw_dir, out_dir)
+    if len(os.listdir(out_dir)) == 0:
+        utils.convert_data_to_bids(raw_dir, out_dir)
 
     # Get NIFTI file paths
-    filePaths = utils.block_reader(out_dir)
-    print("Done preparing BIDS data.")
+    filePaths = utils.nifti_file_paths(out_dir)
 
     # Add variable paths
     os.putenv("$FREESURFER_HOME", "/usr/local/freesurfer")
     os.putenv("$SUBJECTS_DIR", "./data/subjects")
+    os.system("sudo chmod -R a+w $SUBJECTS_DIR")
 
-    # Recon-all every subject
+    # Extract volumes from patients
     subjectId = 0
     for patient in filePaths:
-        print(f"File path: {patient}")
-        print(f"Processing reconstruction of subject {subjectId}.")
         subject = f'sub{subjectId}'
-        os.system(f"recon-all -i {patient} -s {subject} -all")
+        os.system(f"recon-all -i {patient} -s {subject} -all -qcache")
+        utils.export_volumes(os.getenv("SUBJECTS_DIR"), subjectId)
         subjectId += 1
     
 prep_data("./data/ppmi/original", "./data/ppmi/BIDS")
