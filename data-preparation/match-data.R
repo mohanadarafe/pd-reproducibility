3ee02dcb041cd6b61f95930e18f434efa42e08c1
mohanad arafe <mohanad.arafe@munvo.com>
2022-06-14 10:28:08 -0700

    Attempt to merge MRI info and main DF

--- data-preparation/match-data.R
+++
@@ -1,8 +0,0 @@
-library('MatchIt')
-require('MatchIt')
-
-data <- read.csv('../data/volume-data/cohortBeforeMatching')
-matcher <- matchit(group ~ age+gen, data=data, method="nearest", distance="glm", replacement=F)
-final_data = match.data(matcher)
-write.csv(final_data, file="../data/volume-data/matchedVolumes.csv")
-print(summary(matcher))
